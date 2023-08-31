5f19d023de4f0f0854216097a0a706c64dbb3d34
milivojejankovic <fiendish@DESKTOP-EDQ019F>
2023-08-31 15:42:20 +0200

    Comments boiiii

--- src/components/DUMMY_DATA.js
+++ src/components/DUMMY_DATA.js
@@ -6,7 +6,25 @@ export default [
         title: "Vucic budala",
         content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non                              proident, sunt in culpa qui officia  deserunt mollit anim id est laborum.",
         publishedDate: "12.09.2023",
-        rating: 16
+        rating: 16,
+        comments: [
+            {
+                id: '0',
+                author: "marchel0legend",
+                content: "Jebem ti drzawu...",
+                repplyTo: "marchel0legend",
+                publishedDate: "12.09.2023",
+                rating: 2,
+            },
+            {
+                id: '0',
+                author: "djoka33",
+                content: "Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non                              proident, sunt in culpa qui officia  deserunt mollit anim id est laborum.",
+                repplyTo: "marchel0legend",
+                publishedDate: "12.09.2023",
+                rating: 12,
+            }
+        ]
     },
     {
         id: '1',
@@ -33,7 +51,25 @@ export default [
         title: "ja jako volim tenkove :)",
         content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non                              proident, sunt in culpa qui officia  deserunt mollit anim id est laborum.",
         publishedDate: "99.04.2023",
-        rating: 1852
+        rating: 1852,
+        comments: [
+            {
+                id: '0',
+                author: "milanEKSODIJA",
+                content: "JA MNOGO VOLIM DA SEIGRAM SA TENKOVIMA",
+                repplyTo: null,
+                publishedDate: "12.09.2023",
+                rating: 2,
+            },
+            {
+                id: '0',
+                author: "wotBOG",
+                content: "NUBOVIIII.",
+                repplyTo: "milanEKSODIJA",
+                publishedDate: "12.09.2023",
+                rating: -2,
+            }
+        ]
     },
     {
         id: '4',
@@ -51,7 +87,33 @@ export default [
         title: "jel prodaje neko lopatu malu kasikastu za arheologiju. nudim 20din",
         content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non                              proident, sunt in culpa qui officia  deserunt mollit anim id est laborum.",
         publishedDate: "1.02.2021",
-        rating: -58996
+        rating: -58996,
+        comments: [
+            {
+                id: '0',
+                author: "ljubomirlopate2023",
+                content: "Imam ja kolega 22 dinara.",
+                repplyTo: null,
+                publishedDate: "12.09.2023",
+                rating: 2,
+            },
+            {
+                id: '0',
+                author: "m1ndor",
+                content: "a brt mnogo mi to aj za 2 din.",
+                repplyTo: "ljubomirlopate2023",
+                publishedDate: "12.09.2023",
+                rating: -2,
+            },
+            {
+                id: '0',
+                author: "lopate.com",
+                content: "Imamo na sajtu.",
+                repplyTo: "m1ndor",
+                publishedDate: "12.09.2023",
+                rating: 2,
+            },
+        ]
     },
     {
         id: '6',
---
+++ src/components/shared/elements/comment/CommentTeaser.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import ThreadPostMeta from "./ThreadPostMeta";
+import ThreadPostRating from "./ThreadPostRating";
+import DivLink from "../DivLink";
+
+import "./CommentTeaser.css";
+
+const CommentTeaser = (props) => {
+  return <DivLink to={`t/${props.parentThread}/${props.id}`}>
+    <div
+      className={`single-comment parent-thread-${props.parentThread}`}
+      id={`comment-${props.id}`}
+    >
+      <CommentMeta author={props.author} parentThread={props.parentThread} repplyTo={props.repplyTo} date={props.date} />
+      <div className="comment-container">
+        <ThreadPostRating rating={props.rating} />
+          <div className="comment-content">{props.content.substring(0, 150) + "..."}</div>
+      </div>
+    </div>
+  </DivLink>
+
+};
+
+export default CommentTeaser;
